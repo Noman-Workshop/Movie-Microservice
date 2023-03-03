7cac0d738eb07784e8f8cacf54d8f9f07e0a313a
Noman5237 <anonyman637@gmail.com>
2023-03-04 04:32:35 +0600

    Eureka peers config
    Peers need to register to each other

---
+++ apps/movie-catalog/src/main/java/com/cineplex/moviecatalog/MovieCatalogApplication.java
@@ -0,0 +1,25 @@
+package com.cineplex.moviecatalog;
+
+import com.netflix.discovery.DefaultEurekaClientConfig;
+import com.netflix.discovery.EurekaClientConfig;
+import com.netflix.eureka.EurekaServerConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+@EnableEurekaClient
+public class MovieCatalogApplication {
+	
+	public MovieCatalogApplication(EurekaClientConfig config) {
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(MovieCatalogApplication.class, args);
+	}
+
+}
