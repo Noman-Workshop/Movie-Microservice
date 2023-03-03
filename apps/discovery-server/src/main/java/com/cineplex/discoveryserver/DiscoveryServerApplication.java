6e3b3f018343c5f7353399a1bc3943f1d4efd881
Noman5237 <anonyman637@gmail.com>
2023-03-04 05:27:05 +0600

    Removing the timeout

--- apps/discovery-server/src/main/java/com/cineplex/discoveryserver/DiscoveryServerApplication.java
+++ apps/discovery-server/src/main/java/com/cineplex/discoveryserver/DiscoveryServerApplication.java
@@ -1,13 +1,9 @@
 package com.cineplex.discoveryserver;
 
-import com.netflix.appinfo.ApplicationInfoManager;
-import com.netflix.discovery.DiscoveryClient;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EurekaClientConfigBean;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,21 +17,16 @@ import java.util.Map;
 @Configuration
 public class DiscoveryServerApplication {
 	
-	@Autowired
-	ApplicationInfoManager applicationInfoManager;
+	private final EurekaClientConfigBean eurekaClientConfigBean;
 	
-	public static final EurekaClientConfigBean eurekaClientConfigBean = new EurekaClientConfigBean();
-	
-	@Bean
-	public EurekaClientConfigBean eurekaClientConfigBean() {
-		eurekaClientConfigBean.setEurekaServiceUrlPollIntervalSeconds(10);
-		return eurekaClientConfigBean;
+	public DiscoveryServerApplication(EurekaClientConfigBean eurekaClientConfigBean) {
+		this.eurekaClientConfigBean = eurekaClientConfigBean;
 	}
+	
 	
 	// set service url for eureka client
 	@PostMapping ("/setServiceUrl/{serviceUrl}")
 	public void setServiceUrl(@PathVariable String serviceUrl) {
-		DiscoveryClient discoveryClient = new DiscoveryClient(applicationInfoManager, eurekaClientConfigBean);
 		Map<String, String> serviceUrls = eurekaClientConfigBean.getServiceUrl();
 		String existingUrls = serviceUrls
 				.get(EurekaClientConfigBean.DEFAULT_ZONE);
