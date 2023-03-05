0452e3e8ee8c318b3c6eef178bb33c0262a36dc2
Noman5237 <anonyman637@gmail.com>
2023-03-05 13:34:00 +0600

    added auth service for configuration practice

--- apps/movie-catalog/src/main/java/com/cineplex/moviecatalog/MovieCatalogApplication.java
+++ apps/movie-catalog/src/main/java/com/cineplex/moviecatalog/MovieCatalogApplication.java
@@ -1,12 +1,7 @@
 package com.cineplex.moviecatalog;
 
-import com.netflix.discovery.DefaultEurekaClientConfig;
-import com.netflix.discovery.EurekaClientConfig;
-import com.netflix.eureka.EurekaServerConfig;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
