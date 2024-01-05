61db98df8b88fc7bde45d9c7ae0848011667414d
Noman5237 <anonyman637@gmail.com>
2024-01-05 17:06:46 +0600

    discontunuing movie microservice

--- apps/discoveryserver/src/main/java/com/cineplex/discoveryserver/DiscoveryServerApplication.java
+++ apps/discoveryserver/src/main/java/com/cineplex/discoveryserver/DiscoveryServerApplication.java
@@ -4,15 +4,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 <<<<<<< Updated upstream:apps/discoveryserver/src/main/java/com/cineplex/discoveryserver/DiscoveryServerApplication.java
+<<<<<<< Updated upstream:apps/discoveryserver/src/main/java/com/cineplex/discoveryserver/DiscoveryServerApplication.java
 
 @SpringBootApplication
 @EnableEurekaServer
 =======
+=======
+>>>>>>> Stashed changes:apps/discovery-server/src/main/java/com/cineplex/discoveryserver/DiscoveryServerApplication.java
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaServer
 @RestController
+<<<<<<< Updated upstream:apps/discoveryserver/src/main/java/com/cineplex/discoveryserver/DiscoveryServerApplication.java
+>>>>>>> Stashed changes:apps/discovery-server/src/main/java/com/cineplex/discoveryserver/DiscoveryServerApplication.java
+=======
 >>>>>>> Stashed changes:apps/discovery-server/src/main/java/com/cineplex/discoveryserver/DiscoveryServerApplication.java
 public class DiscoveryServerApplication {
 
